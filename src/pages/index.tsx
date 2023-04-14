5e0021b29b76097e5dfbb1f6cf67fbbbb9faa393
wtshek <wingtung.shek@accedo.tv>
2023-04-14 19:14:59 +0800

    feat: add protected route function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="">Netflix Clone</h1>
+      <h1 className="text-white">Netflix Clone</h1>
+      {user?.name}
+      <button className="text-white" onClick={() => signOut()}>
+        Signout!
+      </button>
     </>
   );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return { redirect: { destination: "/auth", permanent: false } };
+  }
+
+  return {
+    props: {},
+  };
 }
