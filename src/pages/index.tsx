e10dc6174267d70bcb72764802b26b3eb4c31920
wtshek <wingtung.shek@accedo.tv>
2023-04-15 21:14:02 +0800

    feat: add movieCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
-import Billboard from "@/components/Billboard";
-import Navbar from "@/components/Navbar";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import Navbar from "@/components/Navbar";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useMovieList from "@/hooks/useMovieList";
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
